0c7da9da5a7b33889558db13dbace7f269cfde0f
krrishnagn <krrishna@gmail.com>
2024-04-07 02:12:45 +0530

    ara kora login screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,63 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, HttpClientModule, RouterOutlet,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Civil';
+export class AppComponent implements OnInit {
+  
+  title = 'CIVILIAN';
+  constructor( private _http : HttpClient , private _router : Router)
+  {
+
+  }
+  
+
+  ngOnInit(): void {
+
+  }
+
+  
+  UserName? : String = "" //"sarojini.ravi@kosoft.co";
+  password? : String = "" // "pass";
+  ErrMsg?: string = "";
+  responseData: any;
+
+  async Login() {
+    if (this.UserName && this.password) {
+      try {
+        const response: any = await this._http.get('https://api.kolims.com/api/Signature/Login/' + this.UserName + '/' + this.password).toPromise();
+        console.log('API Response:', response);
+        this.responseData = response;
+        this.GetLoginCall();
+      } catch (error) {
+        console.error('API Error:', error);
+        this.ErrMsg = "An error occurred while processing your request. Please try again later.";
+      }
+    } else {
+      this.ErrMsg = "Please enter the UserName and Password";
+    }
+  }
+
+  GetLoginCall() {
+    if (this.responseData && this.responseData.data.returnCode === '0') {
+      console.log("Condition passed")
+      this.ErrMsg = "";
+      this._router.navigate(['LoginSuccess']);
+      this.UserName = "";
+      this.password = "";
+    } else {
+      console.log(this.responseData, "waht")
+      this.ErrMsg = "UserName or password is invalid";
+    }
+  }
+
+
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : "",
+        component : AppComponent
+    },{
+        path : "LoginSuccess",
+        component : AppComponent
+    }
+];
